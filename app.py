7a74142068aa79f8a43df7fceb2fb5f634138a44
Roman Willie <rwillie@usf.edu>
2025-11-25 00:13:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,22 +3,24 @@
 #Welcome Message
 print("Welcome to my Python program!")
 
-#User input propmt
+#User input prompt
 def get_study_hours():
     hours = input("How many hours did you study today?")
     return hours
 
 #Calculate weekly study hours
 def calculate_weekly_hours(hours_input):
-    return hours * 7
+    return hours_input * 7
 
 def main():
     try:
         #Receive user input
         hours_input = get_study_hours()
         #Convert user input to float
-        weekly_hours = calculate_weekly_hours(hour)
+        hours = float(hours_input)
+        weekly_hours = calculate_weekly_hours(hours)
         #Display results
+        print(f"\nYou studied {hours:.2f} hours today.") 
         print(f"You are on track to study {weekly_hours} hours this week.")
     except ValueError:
         print("Please enter valid number.")
