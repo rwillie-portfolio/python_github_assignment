e614cc547ec4c19b4a7efa3af75a9d45830c4334
Roman Willie <rwillie@usf.edu>
2025-11-24 23:41:40 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+#app.py
+
+#Welcome Message
+print("Welcome to my Python program!")
+
+#User input propmt
+def get_study_hours():
+    hours = input("How many hours did you study today?")
+    return hours
+
+#Calculate weekly study hours
+def calculate_weekly_hours(hours_input):
+    return hours * 7
+
+def main():
+    try:
+        #Receive user input
+        hours_input = get_study_hours()
+        #Convert user input to float
+        weekly_hours = calculate_weekly_hours(hour)
+        #Display results
+        print(f"You are on track to study {weekly_hours} hours this week.")
+    except ValueError:
+        print("Please enter valid number.")
+        exit()
+
+#Run the function
+if __name__ == "__main__":
+    main()
